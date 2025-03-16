76f592064600f4a8b2bb5ee3d8ce74a2c6071076
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-03-15 22:26:19 -0500

    add funcionality buttons

--- src/environments/environments.dev.ts
+++ src/environments/environments.dev.ts
@@ -1,6 +1,6 @@
 export const environments ={
     CYPHER_KEY: 'W40Y88xYhDV~g4=I0w',
-    URL_CITIES: 'https://countriesnow.space/api/v0.1/countries/cities',
+    URL_TASKS: 'https://run.mocky.io/v3/b8ad6dfa-bc97-495b-aff8-46621dd2de30',
     URL_HOTELS_MNGR: 'http://localhost:8080/v1/api/hotels-mngr',
     URL_API_USERS: 'https://agency-travel.free.beeceptor.com',
 }
